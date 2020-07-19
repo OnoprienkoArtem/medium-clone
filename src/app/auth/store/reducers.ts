fbbf6a4a7a0486427cec07012e2d6f0bbb791f6c
OnoprienkoArtem <o.artem.e@gmail.com>
2020-07-19 16:57:06 +0300

    change status auth interface and reducer

--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -1,10 +1,13 @@
 import { Action, createReducer, on } from '@ngrx/store';
 
 import { AuthStateInterface } from 'src/app/auth/types/authState.interface';
-import { registerAction } from './actions/register.action';
+import { registerAction, registerFailureAction, registerSuccessAction } from './actions/register.action';
 
 const initialState: AuthStateInterface = {
   isSubmitting: false,
+  currentUser: null,
+  isLoggedIn: null,
+  validationErrors: null,
 };
 
 const authReducer = createReducer(
@@ -13,10 +16,28 @@ const authReducer = createReducer(
     registerAction,
     (state): AuthStateInterface => ({
       ...state,
-      isSubmitting: true
-    }
+      isSubmitting: true,
+      validationErrors: null,
+    })
+  ),
+  on(
+    registerSuccessAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      isLoggedIn: true,
+      currentUser: action.currentUser
+    })
+  ),
+  on(
+    registerFailureAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      validationErrors: action.errors
+    })
   )
-));
+);
 
 export function reducers(state: AuthStateInterface, action: Action) {
   return authReducer(state, action);
