de04414c56228db5fead9b57dbeadf6cccd6d0cd
OnoprienkoArtem <o.artem.e@gmail.com>
2020-07-21 23:37:44 +0300

    add get method to persistance service

--- src/app/shared/services/persistance.service.ts
+++ src/app/shared/services/persistance.service.ts
@@ -6,7 +6,16 @@ export class PersistanceService {
     try {
       localStorage.setItem(key, JSON.stringify(data));
     } catch (e) {
-      console.log('Error saving to localStorage', e);
+      console.error('Error saving to localStorage', e);
+    }
+  }
+
+  get(key: string): any {
+    try {
+      return JSON.parse(localStorage.getItem(key));
+    } catch (e) {
+      console.error('Error getting data from localStorage', e);
+      return null;
     }
   }
 }
