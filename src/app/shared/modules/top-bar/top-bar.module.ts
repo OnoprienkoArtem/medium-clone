1f6ea15b28845daff47c56b15e9b48ef47c35c43
OnoprienkoArtem <o.artem.e@gmail.com>
2020-07-24 22:24:00 +0300

    create topbar component

--- src/app/shared/modules/top-bar/top-bar.module.ts
+++ src/app/shared/modules/top-bar/top-bar.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [TopBarComponent],
   imports: [
     CommonModule
   ]
