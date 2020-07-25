9650d50acdf58d04f97434f76f68a53806778aaa
OnoprienkoArtem <o.artem.e@gmail.com>
2020-07-26 00:00:15 +0300

    create var for get data from selectors

--- src/app/shared/modules/top-bar/components/top-bar/top-bar.component.ts
+++ src/app/shared/modules/top-bar/components/top-bar/top-bar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { select, Store } from '@ngrx/store';
+
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
+import { currentUserSelector, isAnonymousSelector, isLoggedInSelector } from 'src/app/auth/store/selectors';
 
 @Component({
   selector: 'mc-top-bar',
@@ -6,10 +11,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-bar.component.scss']
 })
 export class TopBarComponent implements OnInit {
+  isLoggedIn$: Observable<boolean>;
+  isAnonymous$: Observable<boolean>;
+  currentUser$: Observable<CurrentUserInterface | null>;
 
-  constructor() { }
+  constructor(private store: Store) { }
 
   ngOnInit(): void {
+    this.isLoggedIn$ = this.store.pipe(select(isLoggedInSelector));
+    this.isAnonymous$ = this.store.pipe(select(isAnonymousSelector));
+    this.currentUser$ = this.store.pipe(select(currentUserSelector));
   }
 
 }
