cdb48fde815aaa59ca85871e3c6acda1b0770244
OnoprienkoArtem <o.artem.e@gmail.com>
2020-07-29 00:03:10 +0300

    get token from ls in effect

--- src/app/auth/store/effects/getCurrentUser.effect.ts
+++ src/app/auth/store/effects/getCurrentUser.effect.ts
@@ -5,7 +5,6 @@ import { of } from 'rxjs';
 import { catchError, map, switchMap } from 'rxjs/operators';
 import { AuthService } from '../../services/auth.service';
 import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
-import { HttpErrorResponse } from '@angular/common/http';
 import { PersistanceService } from 'src/app/shared/services/persistance.service';
 import { getCurrentUserAction, getCurrentUserFailureAction, getCurrentUserSuccessAction } from '../actions/getCurrentUser.action';
 
@@ -16,11 +15,17 @@ export class GetCurrentUserEffect {
     this.actions$.pipe(
       ofType(getCurrentUserAction),
       switchMap(() => {
+        const token = this.persistanceService.get('accessToken');
+
+        if (!token) {
+          return of(getCurrentUserFailureAction());
+        }
+
         return this.authService.getCurrentUser().pipe(
           map((currentUser: CurrentUserInterface) => {
             return getCurrentUserSuccessAction({currentUser});
           }),
-          catchError((errorResponse: HttpErrorResponse) => {
+          catchError(() => {
             return of(getCurrentUserFailureAction());
           })
         );
