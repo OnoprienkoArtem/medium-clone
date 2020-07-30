0aa7ea789a24e6cd0d1e15400ccf71a6bb4cd3b7
OnoprienkoArtem <o.artem.e@gmail.com>
2020-07-30 23:23:02 +0300

    create basic auth interceptor

---
+++ src/app/auth/services/authinterceptor.service.ts
@@ -0,0 +1,17 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    request = request.clone({
+      setHeaders: {
+        Authorization: ''
+      }
+    });
+
+    return next.handle(request);
+  }
+
+}
