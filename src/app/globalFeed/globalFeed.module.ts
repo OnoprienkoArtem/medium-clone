e295a347673417d115cd03d6dfe70b9e7bfda8de
OnoprienkoArtem <o.artem.e@gmail.com>
2020-07-31 22:42:40 +0300

    create basic global module and component

---
+++ src/app/globalFeed/globalFeed.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GlobalFeedComponent } from './components/global-feed/global-feed.component';
+
+@NgModule({
+  declarations: [GlobalFeedComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GlobalFeedModule { }
