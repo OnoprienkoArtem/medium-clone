b30fdd9710e58311f626fbcdb7ea519aad4d2493
OnoprienkoArtem <o.artem.e@gmail.com>
2020-08-04 00:00:10 +0300

    createarticle interface

--- src/app/shared/modules/feed/services/feed.service.ts
+++ src/app/shared/modules/feed/services/feed.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class FeedService {
 
   constructor() { }
+
+  getFeed(url) {
+
+  }
 }
