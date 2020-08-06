ee9662c5ef4a5fecb21a247561f8c969ecaef5de
OnoprienkoArtem <o.artem.e@gmail.com>
2020-08-06 22:45:57 +0300

    import reducer to feed mofule

--- src/app/shared/modules/feed/feed.module.ts
+++ src/app/shared/modules/feed/feed.module.ts
@@ -4,12 +4,15 @@ import { FeedComponent } from './components/feed/feed.component';
 import { FeedService } from './services/feed.service';
 import { EffectsModule } from '@ngrx/effects';
 import { GetFeedEffect } from './store/effects/getFeed.effect';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './store/reducers';
 
 @NgModule({
   declarations: [FeedComponent],
   imports: [
     CommonModule,
     EffectsModule.forFeature([GetFeedEffect]),
+    StoreModule.forFeature('feed', reducers),
   ],
   exports: [
     FeedComponent,
