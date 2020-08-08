399d4b7436c6e1de9c65ed573444de1893731496
OnoprienkoArtem <o.artem.e@gmail.com>
2020-08-08 22:32:13 +0300

    add observable property to feedcomponent

--- src/app/shared/modules/feed/components/feed/feed.component.ts
+++ src/app/shared/modules/feed/components/feed/feed.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { getFeedAction } from '../../store/actions/getFeed.action';
+import { GetFeedResponseInterface } from '../../types/getFeedResponse.interface';
 
 @Component({
   selector: 'mc-feed',
@@ -8,8 +10,11 @@ import { getFeedAction } from '../../store/actions/getFeed.action';
   styleUrls: ['./feed.component.scss']
 })
 export class FeedComponent implements OnInit {
-
   @Input() apiUrl: string;
+
+  feed$: Observable<GetFeedResponseInterface | null>;
+  error$: Observable<string | null>;
+  isLoading$: Observable<boolean>;
 
   constructor(private store: Store) { }
 
