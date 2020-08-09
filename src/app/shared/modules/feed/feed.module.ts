f33ccecafd2d6ec10f2d19422270d2a72fdf5b7b
OnoprienkoArtem <o.artem.e@gmail.com>
2020-08-09 23:59:34 +0300

    add fedd markap

--- src/app/shared/modules/feed/feed.module.ts
+++ src/app/shared/modules/feed/feed.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FeedComponent } from './components/feed/feed.component';
-import { FeedService } from './services/feed.service';
-import { EffectsModule } from '@ngrx/effects';
-import { GetFeedEffect } from './store/effects/getFeed.effect';
+import { RouterModule } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
+import { FeedService } from './services/feed.service';
+import { GetFeedEffect } from './store/effects/getFeed.effect';
 import { reducers } from './store/reducers';
+
+import { FeedComponent } from './components/feed/feed.component';
 
 @NgModule({
   declarations: [FeedComponent],
   imports: [
     CommonModule,
+    RouterModule,
     EffectsModule.forFeature([GetFeedEffect]),
     StoreModule.forFeature('feed', reducers),
   ],
