fdf89c04bc6b3433f301a87bcf17656c416e31ca
OnoprienkoArtem <o.artem.e@gmail.com>
2020-08-12 23:57:56 +0300

    get route param

--- src/app/shared/modules/feed/components/feed/feed.component.ts
+++ src/app/shared/modules/feed/components/feed/feed.component.ts
@@ -1,18 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { getFeedAction } from '../../store/actions/getFeed.action';
 import { errorSelector, feedSelector, isLoadingSelector } from '../../store/selectors';
 import { GetFeedResponseInterface } from '../../types/getFeedResponse.interface';
 import { environment } from 'src/environments/environment';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'mc-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss']
 })
-export class FeedComponent implements OnInit {
+export class FeedComponent implements OnInit, OnDestroy {
   @Input() apiUrl: string;
 
   feed$: Observable<GetFeedResponseInterface | null>;
@@ -20,15 +20,30 @@ export class FeedComponent implements OnInit {
   isLoading$: Observable<boolean>;
   limit = environment.limit;
   baseUrl: string;
+  queryParamsSubscription: Subscription;
 
   constructor(
     private store: Store,
     private router: Router,
+    private route: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
     this.initializeValues();
+    this.initializeListeners();
     this.fetchData();
+  }
+
+  ngOnDestroy(): void {
+    this.queryParamsSubscription.unsubscribe();
+  }
+
+  initializeListeners(): void {
+    this.queryParamsSubscription = this.route.queryParams.subscribe(
+      (params: Params) => {
+        
+      }
+    );
   }
 
   initializeValues(): void {
