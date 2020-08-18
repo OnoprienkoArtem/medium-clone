3d24e51f42b0c86544949d584087efc14ca473d1
OnoprienkoArtem <o.artem.e@gmail.com>
2020-08-18 23:55:56 +0300

    add ngrx router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { GlobalFeedModule } from './globalFeed/globalFeed.module';
     AppRoutingModule,
     AuthModule,
     HttpClientModule,
-    StoreModule.forRoot({}),
+    StoreModule.forRoot({ router: routerReducer }),
+    StoreRouterConnectingModule.forRoot(),
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
--- src/app/shared/modules/feed/store/reducers.ts
+++ src/app/shared/modules/feed/store/reducers.ts
@@ -1,3 +1,4 @@
+import { routerNavigatedAction } from '@ngrx/router-store';
 import { Action, createReducer, on } from '@ngrx/store';
 
 import { FeedStateInterface } from '../types/feedState.interface';
@@ -37,6 +38,7 @@ const feedReducer = createReducer(
       isLoading: false
     })
   ),
+  on(routerNavigatedAction, (): FeedStateInterface => initialState),
 );
 
 export function reducers(state: FeedStateInterface, action: Action) {
