81e09caf86d181da697cbfc17ab3604626ea61bc
OnoprienkoArtem <o.artem.e@gmail.com>
2020-08-19 23:38:50 +0300

    create tag list component

---
+++ src/app/shared/modules/tagList/components/tagList/tagList.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mc-tag-list',
+  templateUrl: './tagList.component.html',
+  styleUrls: ['./tagList.component.scss']
+})
+export class TagListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/modules/tagList/tagList.module.ts
+++ src/app/shared/modules/tagList/tagList.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TagListComponent } from './components/tag-list/tag-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TagListComponent],
   imports: [
     CommonModule
   ]
