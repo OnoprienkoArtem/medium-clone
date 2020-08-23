a869cb4492814ec60ad32dd3d642de7f2e63bfdc
OnoprienkoArtem <o.artem.e@gmail.com>
2020-08-23 23:14:32 +0300

    replace populartagsstate interface

--- src/app/shared/modules/popularTags/types/popularTagsState.interface.ts
+++ src/app/shared/modules/popularTags/types/popularTagsState.interface.ts
@@ -1,6 +1,6 @@
 import { PopularTagType } from 'src/app/shared/types/popularTag.type';
 
-export interface PopularTagsStateInteface {
+export interface PopularTagsStateInterface {
   data: PopularTagType[] | null;
   isLoading: boolean;
   error: string | null;
