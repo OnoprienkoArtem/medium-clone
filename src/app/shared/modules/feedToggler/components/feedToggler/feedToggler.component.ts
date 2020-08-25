a20fde48276f8de01a46a082aa08f207db8296cc
OnoprienkoArtem <o.artem.e@gmail.com>
2020-08-25 22:15:10 +0300

    create basic module and component for feedtoggler

---
+++ src/app/shared/modules/feedToggler/components/feedToggler/feedToggler.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mc-feed-toggler',
+  templateUrl: './feed-toggler.component.html',
+  styleUrls: ['./feed-toggler.component.scss']
+})
+export class FeedTogglerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/modules/feedToggler/feedToggler.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FeedTogglerComponent } from './components/feedToggler/feedToggler.component';
+
+
+
+@NgModule({
+  declarations: [FeedTogglerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FeedTogglerModule { }
