9000abccac7bfffd02b014dba080d3f56b08f6c2
OnoprienkoArtem <o.artem.e@gmail.com>
2020-08-25 22:22:39 +0300

    create taganme field andselect logged from store

--- src/app/shared/modules/feedToggler/components/feedToggler/feedToggler.component.ts
+++ src/app/shared/modules/feedToggler/components/feedToggler/feedToggler.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { select, Store } from '@ngrx/store';
+import { isLoggedInSelector } from 'src/app/auth/store/selectors';
 
 @Component({
   selector: 'mc-feed-toggler',
-  templateUrl: './feed-toggler.component.html',
-  styleUrls: ['./feed-toggler.component.scss']
+  templateUrl: './feedToggler.component.html',
+  styleUrls: ['./feedToggler.component.scss']
 })
 export class FeedTogglerComponent implements OnInit {
+  @Input() tagName: string;
 
-  constructor() { }
+  isLoading$: Observable<boolean>;
+
+  constructor(private store: Store) { }
 
   ngOnInit(): void {
+    this.initializeValues();
+  }
+
+  initializeValues(): void {
+    this.isLoading$ = this.store.pipe(select(isLoggedInSelector));
   }
 
 }
