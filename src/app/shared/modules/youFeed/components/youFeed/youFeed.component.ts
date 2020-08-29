9cdf84d6846b07d31169364f2ff83061478afcec
OnoprienkoArtem <o.artem.e@gmail.com>
2020-08-29 22:58:32 +0300

    create basic youfeed component and module for it

---
+++ src/app/shared/modules/youFeed/components/youFeed/youFeed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mc-you-feed',
+  templateUrl: './youFeed.component.html',
+  styleUrls: ['./youFeed.component.scss']
+})
+export class YouFeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/modules/youFeed/youFeed.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { YouFeedComponent } from './components/you-feedr/you-feedr.component';
+
+
+
+@NgModule({
+  declarations: [YouFeedComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class YouFeedModule { }
