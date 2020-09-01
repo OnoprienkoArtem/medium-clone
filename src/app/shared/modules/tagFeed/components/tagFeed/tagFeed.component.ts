76c4355241cf2f0429634630c91f5d2425be75ba
OnoprienkoArtem <o.artem.e@gmail.com>
2020-09-01 23:38:37 +0300

    create basic module and component for tagfeed

---
+++ src/app/shared/modules/tagFeed/components/tagFeed/tagFeed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mc-tag-feed',
+  templateUrl: './tagFeed.component.html',
+  styleUrls: ['./tagFeed.component.scss']
+})
+export class TagFeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
