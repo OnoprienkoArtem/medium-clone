1f3ae8b9174c2f38e785156e97eb7ab4d2e55626
OnoprienkoArtem <o.artem.e@gmail.com>
2020-09-03 23:52:01 +0300

    create article module

---
+++ src/app/article/article.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
+
+
+import { ArticleService } from '../shared/services/article.service';
+import { ErrorMessageModule } from '../shared/modules/errorMessage/errorMessage.module';
+import { LoadingModule } from '../shared/modules/loading/loading.module';
+import { PaginationModule } from '../shared/modules/pagination/pagination.module';
+import { TagListModule } from '../shared/modules/tagList/tagList.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ErrorMessageModule,
+    LoadingModule,
+    PaginationModule,
+    TagListModule,
+    EffectsModule.forFeature([ArticleEffect]),
+    StoreModule.forFeature('feed', reducers),
+  ],
+  exports: [
+    ,
+  ],
+  providers: [
+    ArticleService
+  ],
+})
+export class FeedModule { }
