c96cf7350e4d200b6ca0a8615796830b029187c4
OnoprienkoArtem <o.artem.e@gmail.com>
2020-09-16 21:56:00 +0300

    copy from create article folder for edit

---
+++ src/app/editArticle/store/selectors.ts
@@ -0,0 +1,18 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { AppStateInterface } from 'src/app/shared/types/appState.interface';
+import { CreateArticleStateInterface } from '../types/editArticleState.interface';
+
+export const createArticleFeatureSelector = createFeatureSelector<
+  AppStateInterface,
+  CreateArticleStateInterface
+>('createArticle');
+
+export const isSubmittingSelector = createSelector(
+  createArticleFeatureSelector,
+  (createArticleState: CreateArticleStateInterface) => createArticleState.isSubmitting
+);
+
+export const validationErrorsSelector = createSelector(
+  createArticleFeatureSelector,
+  (createArticleState: CreateArticleStateInterface) => createArticleState.validationErrors
+);
