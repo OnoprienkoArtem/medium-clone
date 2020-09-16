9612d4de13cb104c1d43985178d553e7fe1c27c2
OnoprienkoArtem <o.artem.e@gmail.com>
2020-09-16 22:03:16 +0300

    add actionstypes

--- src/app/editArticle/store/actionsTypes.ts
+++ src/app/editArticle/store/actionsTypes.ts
@@ -1,5 +1,9 @@
 export enum ActionTypes {
-  CREATE_ARTICLE = '[Create article] Create Article',
-  CREATE_ARTICLE_SUCCESS = '[Create article] Create Article success',
-  CREATE_ARTICLE_FAILURE = '[Create article] Create Article failure',
+  UPDATE_ARTICLE = '[Update article] Update Article',
+  UPDATE_ARTICLE_SUCCESS = '[Update article] Update Article success',
+  UPDATE_ARTICLE_FAILURE = '[Update article] Update Article failure',
+
+  GET_ARTICLE = '[Update article] Get Article',
+  GET_ARTICLE_SUCCESS = '[Update article] Get Article success',
+  GET_ARTICLE_FAILURE = '[Update article] Get Article failure',
 }
