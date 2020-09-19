52b4895d0edd485cc2fa819b576cc37c3b0b8ae5
OnoprienkoArtem <o.artem.e@gmail.com>
2020-09-19 22:51:05 +0300

    work with edit article component

--- src/app/editArticle/components/editArticle/editArticle.component.ts
+++ src/app/editArticle/components/editArticle/editArticle.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { getArticleAction } from 'src/app/article/store/actions/getArticle.action';
 import { ArticleInputInterface } from 'src/app/shared/types/articleInput.interface';
 import { BackendErrorsInterface } from 'src/app/shared/types/backendErrors.interface';
 import { createArticleAction } from '../../store/actions/updateArticle.action';
@@ -8,26 +10,36 @@ import { isSubmittingSelector, validationErrorsSelector } from '../../store/sele
 
 @Component({
   selector: 'mc-create-article',
-  templateUrl: './createArticle.component.html',
-  styleUrls: ['./createArticle.component.scss']
+  templateUrl: './editArticle.component.html',
+  styleUrls: ['./editArticle.component.scss']
 })
-export class CreateArticleComponent implements OnInit {
+export class EditArticleComponent implements OnInit {
 
-  initialValues: ArticleInputInterface = {
-    title: '',
-    description: '',
-    body: '',
-    tagList: [],
-  };
+  initialValues: Observable<>;
 
+  slug: string;
   isSubmitting$: Observable<boolean>;
   backendErrors$: Observable<BackendErrorsInterface | null>;
 
-  constructor(private store: Store) { }
+  constructor(
+    private store: Store,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.initialValues();
+    this.fetchData();
+    
+  }
+
+  initialValues(): void {
+    this.slug = this.route.snapshot.paramMap.get('slug');
     this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector));
     this.backendErrors$ = this.store.pipe(select(validationErrorsSelector));
+  }
+
+  fetchData(): void {
+    this.store.dispatch(getArticleAction({slug: this.slug}));
   }
 
   onSubmit(articleInput: ArticleInputInterface): void {
