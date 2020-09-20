524426ebccdbf6a8dc80dc3f28f5271c827322a0
OnoprienkoArtem <o.artem.e@gmail.com>
2020-09-20 23:59:22 +0300

    work with edit article component

--- src/app/editArticle/components/editArticle/editArticle.component.ts
+++ src/app/editArticle/components/editArticle/editArticle.component.ts
@@ -3,9 +3,9 @@ import { ActivatedRoute } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { getArticleAction } from 'src/app/article/store/actions/getArticle.action';
+import { isLoadingSelector } from 'src/app/shared/modules/feed/store/selectors';
 import { ArticleInputInterface } from 'src/app/shared/types/articleInput.interface';
 import { BackendErrorsInterface } from 'src/app/shared/types/backendErrors.interface';
-import { createArticleAction } from '../../store/actions/updateArticle.action';
 import { isSubmittingSelector, validationErrorsSelector } from '../../store/selectors';
 
 @Component({
@@ -14,12 +14,11 @@ import { isSubmittingSelector, validationErrorsSelector } from '../../store/sele
   styleUrls: ['./editArticle.component.scss']
 })
 export class EditArticleComponent implements OnInit {
-
-  initialValues: Observable<>;
-
+  initialValues$: Observable<ArticleInputInterface>;
   slug: string;
   isSubmitting$: Observable<boolean>;
   backendErrors$: Observable<BackendErrorsInterface | null>;
+  isLoading$: Observable<boolean>;
 
   constructor(
     private store: Store,
@@ -35,6 +34,7 @@ export class EditArticleComponent implements OnInit {
   initialValues(): void {
     this.slug = this.route.snapshot.paramMap.get('slug');
     this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector));
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector));
     this.backendErrors$ = this.store.pipe(select(validationErrorsSelector));
   }
 
