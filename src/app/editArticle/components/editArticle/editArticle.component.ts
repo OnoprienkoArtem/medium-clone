4b05ad5111929d8460b4384561f55fd619251c40
OnoprienkoArtem <o.artem.e@gmail.com>
2020-09-21 00:07:48 +0300

    done edit component

--- src/app/editArticle/components/editArticle/editArticle.component.ts
+++ src/app/editArticle/components/editArticle/editArticle.component.ts
@@ -2,14 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { getArticleAction } from 'src/app/article/store/actions/getArticle.action';
-import { isLoadingSelector } from 'src/app/shared/modules/feed/store/selectors';
+import { filter, map } from 'rxjs/operators';
+import { getArticleAction } from '../../store/actions/getArticle.action';
+import { ArticleInterface } from 'src/app/shared/types/article.interface';
 import { ArticleInputInterface } from 'src/app/shared/types/articleInput.interface';
 import { BackendErrorsInterface } from 'src/app/shared/types/backendErrors.interface';
-import { isSubmittingSelector, validationErrorsSelector } from '../../store/selectors';
+import { updateArticleAction } from '../../store/actions/updateArticle.action';
+import { isSubmittingSelector, validationErrorsSelector, isLoadingSelector, articleSelector } from '../../store/selectors';
 
 @Component({
-  selector: 'mc-create-article',
+  selector: 'mc-edit-article',
   templateUrl: './editArticle.component.html',
   styleUrls: ['./editArticle.component.scss']
 })
@@ -28,7 +30,6 @@ export class EditArticleComponent implements OnInit {
   ngOnInit(): void {
     this.initialValues();
     this.fetchData();
-    
   }
 
   initialValues(): void {
@@ -36,13 +37,25 @@ export class EditArticleComponent implements OnInit {
     this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector));
     this.isLoading$ = this.store.pipe(select(isLoadingSelector));
     this.backendErrors$ = this.store.pipe(select(validationErrorsSelector));
+    this.initialValues$ = this.store.pipe(
+      select(articleSelector),
+      filter(Boolean),
+      map((article: ArticleInterface) => {
+        return {
+          title: article.title,
+          description: article.description,
+          body: article.body,
+          tagList: article.tagList,
+        };
+      }),
+    );
   }
 
   fetchData(): void {
-    this.store.dispatch(getArticleAction({slug: this.slug}));
+    this.store.dispatch(getArticleAction({ slug: this.slug }));
   }
 
   onSubmit(articleInput: ArticleInputInterface): void {
-    this.store.dispatch(createArticleAction({articleInput}));
+    this.store.dispatch(updateArticleAction({ slug: this.slug, articleInput }));
   }
 }
