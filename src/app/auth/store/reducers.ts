45e1291540011859e59dd47c2ae613ae5d5e801a
OnoprienkoArtem <o.artem.e@gmail.com>
2020-09-25 22:16:53 +0300

    add update current user reducre

--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -4,6 +4,7 @@ import { AuthStateInterface } from 'src/app/auth/types/authState.interface';
 import { getCurrentUserAction, getCurrentUserFailureAction, getCurrentUserSuccessAction } from './actions/getCurrentUser.action';
 import { loginAction, loginFailureAction, loginSuccessAction } from './actions/login.action';
 import { registerAction, registerFailureAction, registerSuccessAction } from './actions/register.action';
+import { updateCurrentUserSuccessAction } from './actions/updateCurrentUser.action';
 
 const initialState: AuthStateInterface = {
   isSubmitting: false,
@@ -88,6 +89,13 @@ const authReducer = createReducer(
       isLoading: false,
       isLoggedIn: false,
       currentUser: null
+    })
+  ),
+  on(
+    updateCurrentUserSuccessAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      currentUser: action.currentUser
     })
   ),
 );
