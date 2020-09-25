db8b3cd93cf2a1aa9d86270371966dbf337fe35c
OnoprienkoArtem <o.artem.e@gmail.com>
2020-09-25 22:22:48 +0300

    create basic module for settings

---
+++ src/app/settings/settings.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const routes = [
+  {
+    path: 'settings',
+    component: ''
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class SettingsModule { }
