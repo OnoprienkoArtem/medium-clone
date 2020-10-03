76e8d01e620b57a2972a4ba4dbcabcc70068324a
OnoprienkoArtem <o.artem.e@gmail.com>
2020-10-03 21:46:17 +0300

    create basic component for addtofavotites

--- src/app/shared/modules/addToFavorites/addToFavorites.module.ts
+++ src/app/shared/modules/addToFavorites/addToFavorites.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddToFavoritesComponent } from './components/add-to-favorites/add-to-favorites.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AddToFavoritesComponent],
   imports: [
     CommonModule
   ]
---
+++ src/app/shared/modules/addToFavorites/components/addToFavorites/addToFavorites.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mc-add-to-favorites',
+  templateUrl: './addToFavorites.component.html',
+  styleUrls: ['./addToFavorites.component.scss']
+})
+export class AddToFavoritesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
