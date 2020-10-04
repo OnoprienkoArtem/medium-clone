2fb38de4ccfca0d9fa0a8e4076669faf15b7ac41
OnoprienkoArtem <o.artem.e@gmail.com>
2020-10-04 23:09:59 +0300

    create effect for favorite

--- src/app/shared/modules/addToFavorites/addToFavorites.module.ts
+++ src/app/shared/modules/addToFavorites/addToFavorites.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddToFavoritesComponent } from './components/addToFavorites/addToFavorites.component';
 import { AddToFavoritesService } from './services/addToFavorites.service';
-
+import { EffectsModule } from '@ngrx/effects';
+import { AddToFavoritesEffect } from './store/effects/addToFavorites.effect';
 
 
 @NgModule({
   declarations: [AddToFavoritesComponent],
   exports: [AddToFavoritesComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    EffectsModule.forFeature([AddToFavoritesEffect]),
   ],
   providers: [AddToFavoritesService]
 })
